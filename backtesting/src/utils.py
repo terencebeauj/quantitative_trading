0031f4a8d9f4284b9ad9d939b77de94197c0487e
Terence <beaujour.t@hotmail.fr>
2022-03-17 22:50:55 -0400

    Create the hdf5 database framework

--- backtesting/src/utils.py
+++ backtesting/src/utils.py
@@ -1,4 +1,13 @@
 import datetime
+import pandas as pd
+
+TF_EQUIV = {"1m": "1Min", "5m": "5Min", "15m": "15Min", "30m": "30Min", "1h": "1H", "4h": "4H", "12h": "12H", "1d": "D"}
 
 def ms_to_dt(ms: int) -> datetime.datetime:
   return datetime.datetime.utcfromtimestamp(ms / 1000)
+
+
+def resample_timeframe(data: pd.DataFrame, tf: str) -> pd.DataFrame:
+  return data.resample(TF_EQUIV[tf]).agg(
+    {"open": "first", "high": "max", "low": "min", "close": "last", "volume": "sum"}
+  )
