95cbae97393a6b3c7b1144751143e41031e0cd19
Terence <beaujour.t@hotmail.fr>
2022-03-21 01:10:07 -0400

    Begin statistical analysis on data

--- backtesting/src/analysis.py
+++ backtesting/src/analysis.py
@@ -14,12 +14,14 @@ def analyzer(symbol):
     return
   
   df = db.get_data(symbol, 0, time.time() * 1000)
+  df = resample_timeframe(df, "15m")
   print(f"Number of duplicated lines: {df.duplicated().sum()}")
   
   returns = pd.DataFrame(df.close.pct_change(1))
   returns.set_index(df.index, inplace=True)
-  returns = returns[3000:]
-  monthly = returns.groupby([returns.index.year.rename("year"), returns.index.month.rename("month")]).mean()
-  monthly.boxplot(column="close", by="month")
+  returns = returns[3000:]*100
+  monthly = returns.groupby([returns.index.dayofweek.rename("day"), returns.index.hour.rename("hour")]).mean()
+  print(monthly)
+  monthly.boxplot(column="close", by="hour")
   plt.show()
 
